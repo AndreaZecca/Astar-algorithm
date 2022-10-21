78aa9def1f6633077b607970b82626ee9114d55e
Andrea Zecca <andreazecca03@gmail.com>
2022-10-21 18:37:32 +0200

    Finished

--- Astar.py
+++ Astar.py
@@ -8,16 +8,16 @@ WIN = pygame.display.set_mode((WIDTH, WIDTH))
 pygame.display.set_caption("A* Path Finding Algorithm Visualization")
 
 COLOR = {
-    "RED": (255,0,0),
-    "GREEN": (0,255,0),
-    "BLUE": (0,0,255),
-    "YELLOW": (255,255,0),
-    "WHITE": (255,255,255),
-    "BLACK": (0,0,0),
-    "PURPLE": (128,0,128),
-    "ORANGE": (255,165,0),
-    "GREY": (128,128,128),    
-    "TURQUOISE": (64,225,208)
+    "RED": (255,0,0), # closed
+    "GREEN": (0,255,0), # open
+    "BLUE": (0,0,255), # path
+    "YELLOW": (255,255,0), # 
+    "WHITE": (255,255,255), # free
+    "BLACK": (0,0,0), # wall
+    "PURPLE": (128,0,128), # end
+    "ORANGE": (255,165,0), # start
+    "GREY": (128,128,128), #  
+    "TURQUOISE": (64,225,208) # end
 }
 
 
@@ -68,6 +68,51 @@ def h(p1, p2):
     x1, y1 = p1
     x2, y2 = p2
     return abs(x1 - x2) + abs(y1 - y2)
+
+def algorithm(draw, grid, start, end):
+    count = 0
+    open_set = PriorityQueue()
+    open_set.put((0,count, start))
+    came_from = {}
+    g_score = {spot: float("inf") for row in grid for spot in row}
+    g_score[start] = 0
+    f_score = {spot: float("inf") for row in grid for spot in row}
+    f_score[start] = h(start.get_pos(), end.get_pos())
+    
+    open_set_hash = {start}
+
+    while not open_set.empty():
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+        current = open_set.get()[2]
+        open_set_hash.remove(current)
+        if current == end:
+            reconstruct_path(came_from, end, draw)
+            end.set_state("PURPLE")
+            return True
+        for neighbor in current.neighbors:
+            temp_g_score = g_score[current] + 1
+            if temp_g_score < g_score[neighbor]:
+                came_from[neighbor] = current
+                g_score[neighbor] = temp_g_score
+                f_score[neighbor] = temp_g_score + h(neighbor.get_pos(), end.get_pos())
+                if neighbor not in open_set_hash:
+                    count += 1
+                    open_set.put((f_score[neighbor], count, neighbor))
+                    open_set_hash.add(neighbor)
+                    neighbor.set_state("GREEN")
+        draw()
+
+        if current != start:
+            current.set_state("RED")
+    return False
+
+def reconstruct_path(came_from, current, draw):
+    while current in came_from:
+        current = came_from[current]
+        current.set_state("BLUE")
+        draw()
 
 def make_grid(rows, width):
     grid = []
@@ -150,6 +195,10 @@ def main(win, width):
                         for spot in row:
                             spot.update_neighbors(grid)
                     algorithm(lambda: draw(win, grid, rows, width), grid, start, end)
+                if event.key == pygame.K_c:
+                    start = None
+                    end = None
+                    grid = make_grid(rows, width)
     pygame.quit()
 
 main(WIN, WIDTH)
